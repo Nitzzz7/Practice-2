53f87b20b75fc520e01229b7d2165afec75832ad
nit30 <niteshrana.rana@gmail.com>
2019-08-06 16:15:20 +0530

    file ready to upload on dev-B branch

--- workB.java
+++ workB.java
@@ -2,5 +2,6 @@ class B{
 public static void main(String args()){
 System.out.println("code writtne by developer B");
 System.out.println("line added for dev-A");
+System.out.println("file edited by dev-A on branch dev-A");
 }
 }
